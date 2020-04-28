da1a41d8a3442327f75bbd4d68b0e259ffb588b9
WANG Zijian <zijian0906@gmail.com>
2020-04-28 21:54:00 +0800

    Add mysql chart

--- chart.sh
+++ chart.sh
@@ -1,4 +1,10 @@
 # !/bin/bash
-helm package tokyo
+if [ -z $1 ]
+then
+    echo -e 'FOLDER missing! Should be bash chart.sh FOLDER'
+    exit 1;
+fi
+
+helm package $1
 mv *.tgz docs
 helm repo index docs --url https://orderyourtokyo.github.io/charts
